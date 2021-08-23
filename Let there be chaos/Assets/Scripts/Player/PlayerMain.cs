7803fc7d009df8a1feaaaf4d14f1cc163955fe16
Froskiie <84577349+Froskiie@users.noreply.github.com>
2021-08-23 13:34:29 +0200

    FIXED MOVEMENT
    - multiplying by delt

--- Let there be chaos/Assets/Scripts/Player/PlayerMain.cs
+++ Let there be chaos/Assets/Scripts/Player/PlayerMain.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class PlayerMain : MonoBehaviour
 {
+
+    // MOVEMENT
     public float movementSpeed;
 
     private Vector2 moveInput;
     private Vector2 moveVelocity;
 
-
     private Rigidbody2D rb;
+
+    // HEALTH
+    public float maxHealth;
+    private float currentHealth;
+
+    private SpriteRenderer sprite;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+
+        currentHealth = maxHealth;
     }
 
     void FixedUpdate()
@@ -22,7 +32,7 @@ public class PlayerMain : MonoBehaviour
         moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         moveInput.Normalize(); // Normalisation du vecteur pour permettre un mouvement horizontal, vertical et diagonal uniforme.
         moveVelocity = new Vector2(moveInput.x * movementSpeed, moveInput.y * movementSpeed);
-        rb.velocity = moveVelocity;
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
     }
 
 }
