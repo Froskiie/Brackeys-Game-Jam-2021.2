e248fb1c5154f136d8f4944e790f02dc465caa40
Armanes <77788908+IbrahimArmanes@users.noreply.github.com>
2021-08-25 14:22:14 +0100

    Revert "AI de base"
    This reverts commit 54e1d6f85d1f9768fd435e678c77325677c67a52.

---
+++ Let there be chaos/Assets/AI1.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI1 : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+    public float retreatDistance;
+
+    public Transform player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
